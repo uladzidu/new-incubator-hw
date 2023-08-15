48033e2fb8b227748cb30b9f81a17b6e387da511
vdukhovnik <vdukhovnik1@gmail.com>
2023-08-15 15:15:54 +0200

    task 2 is finished

--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -3,7 +3,7 @@ import {UsersObjectType, UserType} from './HW2';
 
 type UserList2PropsType = {
 	users: UsersObjectType // пропиши типизацию
-	filterUsers: UserType[] // пропиши типизацию
+	filterUsers: () => void // пропиши типизацию
 };
 
 export const UserList2 = (props: UserList2PropsType) => {
@@ -11,11 +11,11 @@ export const UserList2 = (props: UserList2PropsType) => {
 		<div id={'hw02-users'}>
 			<h2>User List:</h2>
 
-			<button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>
+			<button id={'hw02-filter-button'} onClick={() => props.filterUsers()}>SHOW ME FRIENDS FROM LA</button>
 
 			<ul>
 			  {props.users.myFriends.map((user) => (
-			    <CurrentUser />
+			    <CurrentUser user={user} />
 			  ))}
 			</ul>
 		</div>
